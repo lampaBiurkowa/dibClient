58b3dc942d06e5fb60c8c6aeb1c5aa17e021a774
Darek <mrocznaklawiatura@gmail.com>
2019-09-19 21:10:34 +0200

    Initial commit

---
+++ DIBVERHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DibClient
+{
+    static class DIBVERHandler
+    {
+        const char SEPARATOR = ':';
+
+        public static Dictionary<string, int> GetVersions(string path)
+        {
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] components = line.Split(SEPARATOR);
+                if (components.Length != 2)
+                {
+                    System.Console.WriteLine($"DIBVERHandler: Warning: Cannot parse line {line}");
+                    continue;
+                }
+
+                string appName = components[0];
+                if (output.ContainsKey(appName))
+                {
+                    System.Console.WriteLine($"DIBVERHandler: Warning: app {appName} defined more than once");
+                    continue;
+                }
+
+                int.TryParse(components[1], out int version);
+                output.Add(appName, version);
+            }
+
+            return output;
+        }
+
+        public static void saveVersions(string path, Dictionary<string, int> versionsData)
+        {
+            List<string> entries = new List<string>();
+
+            foreach (KeyValuePair<string, int> pair in versionsData)
+                entries.Add($"{pair.Key}{SEPARATOR}{pair.Value}");
+
+            File.WriteAllLines(path, entries);
+        }
+    }
+}
---
+++ DibClient.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+
+namespace DibClient
+{
+    public static class DibClient
+    {
+        const string ZIPS_DIRECTORY_PATH = "dibUpdater";
+
+        static Dictionary<string, int> versionsData = new Dictionary<string, int>();
+        static readonly HttpClient httpClient = new HttpClient();
+
+        static DibClient()
+        {
+            if (!Directory.Exists(ZIPS_DIRECTORY_PATH))
+                Directory.CreateDirectory(ZIPS_DIRECTORY_PATH);
+        }
+
+        public static async void UpdateToMaster(string appName, bool force = false)
+        {
+            try
+            {
+                string uri = $"https://localhost:5001/api/dibDownload/{appName}";
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                byte[] responseBody = await response.Content.ReadAsByteArrayAsync();
+
+                MemoryStream stream = new MemoryStream(responseBody);
+                ZipArchive zipArchive = new ZipArchive(stream);
+
+                string destinationPath = $"{ZIPS_DIRECTORY_PATH}/{appName}";
+                Directory.Delete(destinationPath, true);
+                zipArchive.ExtractToDirectory(destinationPath);
+
+                stream.Dispose();
+            }
+            catch (HttpRequestException e)
+            {
+                System.Console.WriteLine($"zal :{e.Message}");
+            }
+        }
+    }
+}
